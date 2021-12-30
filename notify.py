515a080d4c8e6b96d2fb9871dc4190faed87f61d
Viet-Anh Duong <vietanhs0817@gmail.com>
2021-12-30 11:43:01 +0700

    Add notify workflow
    Signed-off-by: Viet-Anh Duong <vietanhs0817@gmail.com>

--- notify.py
+++ notify.py
@@ -35,15 +35,54 @@ def parse_payload(key: str) -> dict:
       return None
     return json.loads(raw)
 
+def handle_needs_context(key: str, repo_name: str) -> str:
+  """
+    needs_context format:
+      {
+        "test": {
+          "result": "success",
+          "outputs": {}
+        },
+        "docker": {
+          "result": "failure",
+          "outputs": {}
+        }
+      }
+  """ 
+  needs_context = parse_payload(key) 
+  icons = {
+    "success": "✅",
+    "failure": "❌",
+    "cancelled": "❗️",
+    "skipped": "🚫",
+    "unknown": "⚠️"
+  }  
+  messages = []
+  for key, value in needs_context.items():
+    result = value.get("result", "unknown")
+    messages.append(f"*{key}*: {icons.get(result, 'unknown')} {result}") 
+  
+  return '\n'.join(messages)
+
+def format_gitlab_url(git_url: str) -> str:
+  if git_url.endswith('.git'):
+   git_url = git_url[:-len('.git')] 
+ 
+  if git_url.startswith("https://"):
+   git_url = git_url[:-len('https://')] 
+  
+  return f"https://{git_url}"
+  
+def format_commit_hash(commit_hash: str) -> str:
+  if len(commit_hash) < 7:
+    return commit_hash
+  return commit_hash[:7]
+
 TOKEN = env("TELEGRAM_TOKEN")
 GROUP_ID = env("TELEGRAM_GROUP_ID")
 
 RUN_ID = env("GITHUB_RUN_ID")
 GITHUB_REPOSITORY = env("GITHUB_REPOSITORY")
-
-CLIENT_PAYLOAD = parse_payload("CLIENT_PAYLOAD")
-NEEDS_CONTEXT = parse_payload("NEEDS_CONTEXT")
-
 
 len(TOKEN) or err("$TELEGRAM_TOKEN is required", exit_code=1)
 len(GROUP_ID) or err("$TELEGRAM_GROUP_ID is required", exit_code=1)
@@ -51,9 +90,19 @@ len(GROUP_ID) or err("$TELEGRAM_GROUP_ID is required", exit_code=1)
 TELEGRAM_URI = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 WORKFLOW_URL = f"https://github.com/{GITHUB_REPOSITORY}/actions/runs/{RUN_ID}"
 
+client_payload = parse_payload("CLIENT_PAYLOAD")
+repo_name = client_payload.get("repo_name")
+git_url = client_payload.get("git_url")
+commit_message = client_payload.get("commit_message")
+commit_hash = client_payload.get("sha")
 
-template = f"""
+template = f"""**[Bridge CI]** {repo_name}
+*Commit message*: {commit_message}
+*Commit hash*: [{format_commit_hash(commit_hash)}]({format_gitlab_url(git_url)}/-/commit/{commit_hash})
 ---
+{handle_needs_context("NEEDS_CONTEXT", repo_name)}
+---
+*Workflow:* [View Workflow]({WORKFLOW_URL})
 """
 
 payload = {
